73b9118883366af9444db0dcc197773a2772e0d2
Aung Ye Htet <aung.atg1738@gmail.com>
2025-06-07 17:28:24 +0630

    Format:Save

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [],
+  exports: [], // Export any providers or modules if needed
+  // Optionally, you can add global guards, interceptors, or filters here
+})
+export class AuthModule {}
