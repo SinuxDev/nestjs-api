5a1034cb33156b181bcfec4f47fc8655eeed64f2
Aung Ye Htet <aung.atg1738@gmail.com>
2025-06-08 17:01:50 +0630

    JWT Strategy With Passport && Token for [Login]

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,16 @@ import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto, LoginDto } from 'src/dto/auth.dto';
 import * as argon from 'argon2';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
   async login(loginDto: LoginDto) {
     try {
@@ -14,6 +20,7 @@ export class AuthService {
           email: loginDto.email,
         },
         select: {
+          id: true,
           name: true,
           email: true,
           password: true,
@@ -34,14 +41,30 @@ export class AuthService {
       }
 
       return {
+        id: user.id,
         name: user.name,
         email: user.email,
+        token: await this.signToken(user.id, user.email),
       };
     } catch (error) {
       throw error instanceof ForbiddenException
         ? error
         : new ForbiddenException('An unexpected error occurred');
     }
+  }
+
+  signToken(userID: string, email: string): Promise<string> {
+    const data = {
+      sub: userID,
+      email,
+    };
+
+    const secret = this.config.get<string>('JWT_SECRET');
+
+    return this.jwt.signAsync(data, {
+      expiresIn: '1h',
+      secret: secret,
+    });
   }
 
   async signup(dto: AuthDto) {
